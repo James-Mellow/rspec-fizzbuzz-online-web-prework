9e96a99cd948b7a4e48d649da1acc9b252d2bb2a
Matthew Muldrow <jmatthewm92@gmail.com>
2019-03-16 01:10:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,4 +2,4 @@
 define #fizzbuzz()
 if (theNumber is divisible by 3) then print "Fizz"
 else if (theNumber is divisible by 5) then print "Buzz"
-else (theNumber is )
+else /*theNumber is not divisible by 3 or 5
